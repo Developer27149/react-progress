33b004c524a5c5e9ad4ce23a25ea561b307a489f
youyiqin <564151947@qq.com>
2021-09-09 00:32:31 +0800

    add: some codes

--- src/ProgressA/index.jsx
+++ src/ProgressA/index.jsx
@@ -1,17 +1,60 @@
-import { Box } from '@chakra-ui/layout'
-import React, { useState } from 'react'
-import './style.scss'
+import { Box, Text } from '@chakra-ui/layout';
+import React, { useEffect, useRef, useState } from 'react';
+import './style.scss';
 
-export default function ProgressA(props) {  
-  const {size = "40px", progress, stokenW, circleOneStroke, circleTwoStroke} = props
-  
+export default function ProgressA(props) {
+  const {
+    size = 300,
+    progress = 55,
+    stokenW = 9,
+    circleOneStroke = 'white',
+    circleTwoStroke = 'skyblue',
+  } = props;
+  const center = size / 2;
+  const radiu = center / 2 - stokenW / 2;
+  const circumference = 2 * Math.PI * radiu;
+  const [offset, setOffset] = useState(0);
+  const svgRef = useRef();
+  const [scoopProgress, setScoopProgress] = useState(0);
+  useEffect(() => {
+    
+  },[progress])
+  useEffect(() => {
+    const progressOffset = ((100 - progress) / 100) * circumference; // 百分比乘以圆周，等于当前百分比对应的显示部分值
+    setOffset(progressOffset);
+    svgRef.current.style = 'transition: stroke-dashoffset 850ms ease-in-out;';
+  }, [setOffset, circumference, progress, offset]);
   return (
     <Box>
       <svg className="svg" width={size} height={size}>
-        <circle className="svg-circle-bg" stroke={} cx={} cy={} r={} strokeWidth={stokenW}/>
-        <circle className="svg-circle" stroke={} cx={} cy={} r={} strokeWidth={stokenW}/>
-        <text className="svg-circle-text" x={} y={}>{`${progress}%`}</text>
+        <circle
+          className="svg-circle-bg"
+          stroke={circleOneStroke}
+          cx={center}
+          cy={center}
+          r={radiu}
+          strokeWidth={stokenW}
+        />
+        <circle
+          className="svg-circle"
+          stroke={circleTwoStroke}
+          cx={center}
+          cy={center}
+          r={radiu}
+          strokeWidth={stokenW}
+          strokeDasharray={circumference}
+          strokeDashoffset={offset}
+          ref={svgRef}
+        />
+        <text
+          className="svg-circle-text"
+          x={center}
+          y={center}
+          dominantBaseline="middle"
+        >
+          {progress}%
+        </text>
       </svg>
     </Box>
-  )
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { ChakraProvider } from "@chakra-ui/react";
-import ProgressA from "./ProgressA/index.jsx";
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import { ChakraProvider, Button } from '@chakra-ui/react';
+import ProgressA from './ProgressA/index.jsx';
 
-function App({ Component }) {
+function App() {
   // 2. Use at the root of your app
+  const [progress, setProgress] = useState(0);
   return (
     <ChakraProvider>
-      <ProgressA />
+      <Button
+        onClick={() => setProgress(progress + 10)}
+        m="100px auto"
+        display="block"
+      >
+        +10
+      </Button>
+      <ProgressA progress={progress} />
+      <Button
+        onClick={() => setProgress(progress - 20)}
+        m="100px auto"
+        display="block"
+      >
+        -20
+      </Button>
     </ChakraProvider>
   );
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
